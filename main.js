4cc184dafefb5ffb2d087e4ae1bd5a00efaaf672
Ambar2010 <89517719+Ambar2010@users.noreply.github.com>
2022-07-22 18:13:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function preload() {
 	world_start = loadSound("world_start.wav");
 	mario_jump = loadSound("jump.wav");
 	mario_coin = loadSound("coin.wav");
-	mario_kill = loadSound("kick_wav");
+	mario_kill = loadSound("kick.wav");
 	mario_gameover = loadSound("gameover.wav");
 	mario_die = loadSound("mariodie.wav");
 
